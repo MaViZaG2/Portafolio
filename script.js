6de800e12faf8fcbe3041b66bdc43237f7a66849
mavizag_uchiha <mvzg19@gmail.com>
2026-01-17 21:43:38 -0500

    Update script.js
    JS, aumento de imagenes y mejoras del carrusel, ademas de cambio de "imagen" a "slides"

--- script.js
+++ script.js
@@ -9,44 +9,79 @@ let currentIndex = 0;
 const projects = {
   p1: {
     title: "Sistema de Reservas – Salón de Belleza",
-        desc: `
-Aplicación web desarrollada para gestionar reservas de un salón de belleza de forma digital.
-Los clientes pueden seleccionar servicios, visualizar precios y duración, reservar un horario 
-disponible y confirmar su cita mediante WhatsApp.
-El sistema calcula automáticamente la duración total de los servicios y bloquea horarios para evitar reservas cruzadas.
-Incluye un panel de administración para visualizar las citas en tiempo real, con persistencia 
-de datos en Firebase Firestore y despliegue en producción con Firebase Hosting.
-    `,
-    images: [
-      "img/gato.jpg",
-      "img/perro.jpg",
-      "img/capibara.jpg",
-      "img/equisde.jpg",
-
+    slides: [
+      {
+        img: "img/SirellaSpa2.jpg",
+        text: "Vista principal del sistema donde el cliente selecciona servicios y horarios disponibles."
+      },
+      {
+        img: "img/SirellaSpa3.jpg",
+        text: "El usuario pone su nombre y elige la fecha y hora de su cita. Esta válida a fechas no anteriores a la actual y en las que el Spa se encuentre activo, además de evitar cruces de citas con otro usuario."
+      },
+      {
+        img: "img/SirellaSpa4.jpg",
+        text: "El usuario confirma la cita con un adelanto, sale el metodo de pago para esto."
+      },
+      {
+        img: "img/SirellaSpa5.jpg",
+        text: "Una vez registrado el pago se confirma la reserva y se muestran los datos para acercarse, además de facilitar el WhatsApp de la dueña del Spa"
+      },
+      {
+        img: "img/AdminSpa1.jpg",
+        text: "El dueño del Spa tiene una web aparte donde puede ver las reservas que hay y su ingreso es mediante un login con un usario y contraseña"
+      },
+      {
+        img: "img/AdminSpa2.jpg",
+        text: "Una vez iniciada la sesión saldrán todas las reservas que hay, estas se actualizan a medida de que se van ingresando nuevas, además de poder eliminarlas en caso alguien cancele y una vez pasa la fecha de la cita, esta se elimina automaticamente"
+      },
+      {
+        img: "img/AdminSpa3.jpg",
+        text: "De igual manera se lleva un registro en Firebase DataBase para que quede guardado en caso de alguna emergencia"
+      }
     ]
   },
 
   p2: {
     title: "Portafolio personal",
-    desc: `
-Aplicación web desarrollada desde cero como portafolio personal, diseñada para presentar mi perfil académico, 
-habilidades técnicas y proyectos desarrollados.
-La web es una landing page interactiva, con componentes reutilizables y diseño responsive, 
-enfocada en una experiencia de usuario clara y elegante.
-    `,
-    images: [
-      "img/quak.webp",
+    slides: [
+      {
+        img: "img/quak.webp",
+        text: "Landing page desarrollada desde cero para presentar mi perfil académico y profesional."
+      }
+    ]
+  },
+  p3: {
+    title: "Web Snover´s Store",
+    slides: [
+      {
+        img: "img/Snovers-1.jpg",
+        text: "Catálogo completo de la tienda donde pueden filtrar según lo que buscan"
+      },
+      {
+        img: "img/Snovers-2.jpg",
+        text: "Aparece solo el tipo de producto filtrado"
+      },
+      {
+        img: "img/Snovers-3.jpg",
+        text: "Descripción completa del producto, con detalles como tamaño y precio"
+      }
     ]
   }
+
 };
+
+function renderSlide() {
+  const slide = currentProject.slides[currentIndex];
+  modalImg.src = slide.img;
+  modalDesc.textContent = slide.text;
+}
 
 function openProject(key) {
   currentProject = projects[key];
   currentIndex = 0;
 
   modalTitle.textContent = currentProject.title;
-  modalDesc.textContent = currentProject.desc;
-  modalImg.src = currentProject.images[currentIndex];
+  renderSlide();
 
   modal.style.display = "flex";
 }
@@ -56,13 +91,13 @@ function closeProject() {
 }
 
 function nextImage() {
-  currentIndex = (currentIndex + 1) % currentProject.images.length;
-  modalImg.src = currentProject.images[currentIndex];
+  currentIndex = (currentIndex + 1) % currentProject.slides.length;
+  renderSlide();
 }
 
 function prevImage() {
   currentIndex =
-    (currentIndex - 1 + currentProject.images.length) %
-    currentProject.images.length;
-  modalImg.src = currentProject.images[currentIndex];
+    (currentIndex - 1 + currentProject.slides.length) %
+    currentProject.slides.length;
+  renderSlide();
 }
